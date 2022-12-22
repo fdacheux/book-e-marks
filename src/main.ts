1b7d9b58f43676eeedfa738c0a54ec3a0d2e0d4a
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-22 22:38:23 +0100

    feat(components): add footer and header folders

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/main.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-import "./assets/main.css";
-
+library.add(faGithub);
 const app = createApp(App);
 
 app.use(router);
-
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
