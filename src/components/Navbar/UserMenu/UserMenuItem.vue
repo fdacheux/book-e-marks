5f97d36b8a317cd8c3b8b3ef1b5f8ae45457744b
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-12-29 14:33:52 +0100

    feat(usermenu item): create useer menu component

---
+++ src/components/Navbar/UserMenu/UserMenuItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <ul>
+    <li>Profile</li>
+    <li>Modify profile</li>
+    <li>My bookmarks</li>
+    <li>Create bookmark</li>
+    <li>Change language</li>
+  </ul>
+</template>
+<style scoped src="./UserMenuItem.css"></style>
