0ee31056191ec2579aea5086581429a86199c09c
Fanny Dacheux <fanny.dacheux@gmail.com>
2023-01-06 00:16:34 +0100

    feat(signup mail input): add email verifications

--- src/components/AuthItem/SignupForm/SignupFormItem.vue
+++ src/components/AuthItem/SignupForm/SignupFormItem.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import ErrorItem from "../../ErrorItem/ErrorItem.vue";
 import { postUser } from "@/stores/postUser";
-import { ref } from "vue";
+import { ref, type Ref } from "vue";
 
+const isEmailInvalid: Ref<undefined | boolean> = ref(undefined);
 const isPasswordHidden = ref(true);
 const isErrorDisplayed = ref(false);
+
 const url = "/auth/signup";
 const errorMessage =
   "Something went wrong during subscription, please try again later.";
@@ -15,6 +17,32 @@ interface IUserData {
 
 const userForm = <IUserData>{};
 
+const MAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w+)+$/;
+const SEVERAL_AT_SIGN = /(\w*@\w*){2,}/;
+
+const controlEmailValidity = () => {
+  if (userForm.email.length < 1) {
+    isEmailInvalid.value = undefined;
+  } else if (
+    !MAIL_REGEX.test(userForm.email) ||
+    SEVERAL_AT_SIGN.test(userForm.email)
+  ) {
+    isEmailInvalid.value = true;
+  } else {
+    isEmailInvalid.value = false;
+  }
+};
+
+const addClass = (condition: boolean | undefined) => {
+  if (condition === undefined) {
+    return "";
+  } else if (condition) {
+    return "invalid";
+  } else {
+    return "valid";
+  }
+};
+
 const submitUserForm = (userForm: IUserData) => {
   try {
     console.log(userForm);
@@ -24,14 +52,24 @@ const submitUserForm = (userForm: IUserData) => {
   }
 };
 </script>
+
 <template>
   <div class="signup-form_container">
-    <h2>Create account</h2>
-    <form class="signup_form form m-5">
-      <label for="email" class="form-label">Email address: </label>
+    <h1 class="text-center page-title">Create account</h1>
+    <form class="signup_form form">
+      <div class="label-error_container">
+        <label for="email" class="form-label">Email address : </label>
+        <div v-if="isEmailInvalid" class="error-alert">
+          <p class="error-message">Email is invalid</p>
+        </div>
+      </div>
+
       <div class="input-group">
         <span class="input-group-text"
-          ><font-awesome-icon icon="fa-solid fa-envelope"
+          ><font-awesome-icon
+            icon="fa-solid fa-envelope"
+            :class="addClass(isEmailInvalid)"
+            class="icon"
         /></span>
         <input
           type="email"
@@ -39,11 +77,17 @@ const submitUserForm = (userForm: IUserData) => {
           id="email"
           placeholder="franz.kafka@mail.com"
           class="form-control"
+          :class="addClass(isEmailInvalid)"
           required
           v-model="userForm.email"
+          @change="controlEmailValidity()"
+          @keydown="controlEmailValidity()"
         />
       </div>
-      <label for="password" class="form-label">Password: </label>
+
+      <label for="password" class="form-label form-label--margin"
+        >Password :
+      </label>
       <div class="input-group">
         <span class="input-group-text"
           ><font-awesome-icon icon="fa-solid fa-lock"
@@ -62,12 +106,17 @@ const submitUserForm = (userForm: IUserData) => {
           class="input-group-text"
           @mousedown="isPasswordHidden = false"
           @mouseup="isPasswordHidden = true"
+          @touchstart="isPasswordHidden = false"
+          @touchend="isPasswordHidden = true"
           ><font-awesome-icon icon="fa-solid fa-eye"
         /></span>
       </div>
-      <div class="col-12">
+
+      <div class="btn_container">
         <button
           class="btn btn-primary"
+          :class="addClass(isEmailInvalid)"
+          :disabled="isEmailInvalid || isEmailInvalid === undefined"
           type="submit"
           @click.prevent="submitUserForm(userForm)"
         >
@@ -75,11 +124,13 @@ const submitUserForm = (userForm: IUserData) => {
         </button>
       </div>
     </form>
-    <div class="signup_link">
+    <div class="signup_link text-center">
       <p>Already have an account ? :</p>
-      <router-link to="/auth">Log-in</router-link>
+      <router-link active-class="not-active-link" to="/auth"
+        >Sign-in</router-link
+      >
     </div>
     <ErrorItem :message="errorMessage" :url="url" v-if="isErrorDisplayed" />
   </div>
 </template>
-<style scoped></style>
+<style scoped src="./SignupFormItem.css"></style>
--- src/views/Auth/AuthView.vue
+++ src/views/Auth/AuthView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="signup-form_container">
-    <h1>Authentication</h1>
+  <section class="signup-form_container">
     <RouterView></RouterView>
-  </div>
+  </section>
 </template>
